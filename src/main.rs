8a695cdc33ee92ce961a49169bc9b52a3340f5cc
izzunnafis <izzun1412nafis@gmail.com>
2024-01-07 12:32:59 +0700

    add-collect command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
 fn main() {
-    println!("Hello, world!");
+    let cmd_args: Vec<String> = std::env::args().collect();
+
+    let query = &cmd_args[1];
+    let filename = &cmd_args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
