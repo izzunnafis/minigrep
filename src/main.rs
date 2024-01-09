0700f7854d90eed03289faf1fcd826cc61f5ad8e
izzunnafis <izzun1412nafis@gmail.com>
2024-01-09 10:14:03 +0700

    change: output to standar error

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ fn main() {
     let cmd_args: Vec<String> = std::env::args().collect();
 
     let args_config = ArgsConfig::build(&cmd_args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
     if let Err(e) = minigrep::run(args_config) { 
-        println!("Cannot read the file: {}", e);
+        eprintln!("Cannot read the file: {}", e);
         std::process::exit(1);
     }
 }
